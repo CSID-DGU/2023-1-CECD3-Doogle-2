31e28084d4d17bb91565d256d38af7fe7e871ddb
FabioCannavaro <daehan1097@gmail.com>
2023-11-07 01:30:22 +0900

    admin version

--- videochat/src/server.js
+++ videochat/src/server.js
@@ -2,6 +2,8 @@ import http from "http";
 import WebSocket from "ws";
 import express from "express";
 import path from 'path';
+import {Server} from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 
 const __dirname = path.resolve();
 const app = express();
@@ -12,37 +14,89 @@ app.use("/public", express.static(path.join(__dirname, "src", "public")));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true,
+      },
+});
 
-const wss = new WebSocket.Server({server});
-
-function onSocketClose() {
-    console.log("Disconnected from the Browser ❌");
-  }
-  
-const sockets = [];
-  
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "unKnown";
-    console.log("Connected to Browser ✅");
-    socket.on("close", onSocketClose);
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type){
-            case "new_message":
-                sockets.forEach((aSocket) =>
-                  aSocket.send(`${socket.nickname}: ${message.payload}`)
-                );
-                break
-            case "nickname":
-                socket["nickname"] = message.payload;
-                break
-   
-        }
-    });
+instrument(io, {
+    auth: false,
   });
+function publicRooms() {
+    const {
+      sockets: {
+        adapter: { sids, rooms },
+      },
+    } = io;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+      if (sids.get(key) === undefined) {
+        publicRooms.push(key);
+      }
+    });
+    return publicRooms;
+  }
 
-server.listen(3000, handleListen);
+  function countRoom(roomName) {
+    return io.sockets.adapter.rooms.get(roomName)?.size;
+  }
+
+io.on("connection", (socket) => {
+    socket["nickname"] = "UnKnown";
+    socket.onAny((event) => {
+        console.log(`Socket Event: ${event}`);
+      });
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+        io.sockets.emit("room_change", publicRooms());
+
+    });
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) =>
+        socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
+        );
+    });
+    socket.on("disconnect", () => {
+        io.sockets.emit("room_change", publicRooms());
+      });
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        done();
+    });
+    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+});
+
+//const wss = new WebSocket.Server({server});
+
+  
+// const sockets = [];
+  
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "unKnown";
+//     console.log("Connected to Browser ✅");
+//     socket.on("close", onSocketClose);
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch (message.type){
+//             case "new_message":
+//                 sockets.forEach((aSocket) =>
+//                   aSocket.send(`${socket.nickname}: ${message.payload}`)
+//                 );
+//                 break
+//             case "nickname":
+//                 socket["nickname"] = message.payload;
+//                 break
+   
+//         }
+//     });
+//   });
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
